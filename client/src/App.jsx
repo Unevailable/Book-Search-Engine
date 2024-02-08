b72f076d21ee6f3837fad54637484a0f6aabde23
Al Amin <al.amin003@hotmail.com>
2024-02-08 01:00:32 -0500

    fixed bugs in the client files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import ApolloClient from 'apollo-boost'
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
@@ -22,16 +20,10 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<SearchBooks />} />
-            <Route path='/saved' element={<SavedBooks />} />
-            <Route path='*' element={<h1 className='display-2'>Page not found!</h1>} />
-          </Routes>
-        </>
-      </Router>
+ 
+      <Navbar />
+      <Outlet />
+
     </ApolloProvider>
   );
 }
--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import {
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  Row
+} from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';  
-import { DELETE_BOOK } from '../utils/mutations';  
+import { REMOVE_BOOK } from '../utils/mutations';  
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);  
-  const [deleteBook, { error }] = useMutation(DELETE_BOOK); 
+  const [deleteBook, { error }] = useMutation(REMOVE_BOOK); 
   
   const userData = data?.getUserData || {}; 
 
@@ -39,18 +46,18 @@ const SavedBooks = () => {
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <div fluid className='text-light bg-dark'>
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
-      </Jumbotron>
+      </div>
       <Container>
         <h2>
           {userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
-        <CardColumns>
+        <Row>
           {userData.savedBooks.map((book) => (
             <Card key={book.bookId} border='dark'>
               {book.image && <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />}
@@ -64,7 +71,7 @@ const SavedBooks = () => {
               </Card.Body>
             </Card>
           ))}
-        </CardColumns>
+        </Row>
       </Container>
     </>
   );
