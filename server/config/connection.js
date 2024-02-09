e946745a3cd8e868551b766df0099629e54b3edf
Al Amin <al.amin003@hotmail.com>
2024-02-09 09:48:31 -0500

    changes tot connection.

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/book-serach-db',
   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     dbName: "book-search-db",
   }
 );
